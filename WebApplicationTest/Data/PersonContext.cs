2ffda756cba75d38f16dba6c96b48d34937c2f3a
EduardoBerte <duduberte@hotmail.com>
2025-02-10 11:55:34 -0300

    Primeiro commit

---
+++ WebApplicationTest/Data/PersonContext.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Person.Models;
+
+namespace Person.data;
+
+ 
+
+public class PersonContext : DbContext
+{
+    public DbSet<PersonModel> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(connectionString: "Data Source=person.sqlite");
+        base.OnConfiguring(optionsBuilder);
+    }
+
+
+}
